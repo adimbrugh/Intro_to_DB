0a035a6fec5f437e2de2349bfce261223613c553
adimbrugh <haisamismail71@gmail.com>
2024-10-17 16:21:30 +0400

    Added Gateway to Data Adventure! File

--- MySQLServer.py
+++ MySQLServer.py
@@ -17,7 +17,7 @@ def create_database():
             mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_stor' created successfully!")
         
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
