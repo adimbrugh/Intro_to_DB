fbac701d94bb5787f73c14a19bc94187a13da40c
adimbrugh <haisamismail71@gmail.com>
2024-10-17 14:41:02 +0400

    A Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,33 +6,33 @@ USE alx_book_store;
 
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY ,
-    author_name VARCHAR(30) NOT NULL 
-);
-
-
-CREATE TABLE Customers (
-    Customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    Customer_name VARCHAR(30) NOT NULL,
-    email VARCHAR (50) NOT NULL,
-    address TEXT NOT NULL
+    author_name VARCHAR(215) NOT NULL 
 );
 
 
 CREATE TABLE BookS (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    authors_id INT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (authors_id) REFERENCES Authors(authors_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR (215) NOT NULL,
+    address TEXT  
 );
 
 
 CREATE TABLE Orders (
     Order_id INT AUTO_INCREMENT PRIMARY KEY,
-    Customer_id INT,
+    customer_id INT,
     Order_date DATE NOT NULL,
-    FOREIGN KEY (Customer_id) REFERENCES Customers(Customer_id)
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
 
@@ -40,7 +40,7 @@ CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     Order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL,
     FOREIGN KEY (Order_id) REFERENCES Orders(Order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE NOT NULL
 );
